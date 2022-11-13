739f94e61a0192c6ab08eb95bec517d51038e98e
Sourav Dutta <sourav.equilizer007@gmail.com>
2022-11-14 02:43:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'http-request-practice';
+  loadedPosts = [];
+  @ViewChild('postForm', { static: false }) postData: NgForm;
+
+  onCreatePost(postData: { title: string, content: string }) {
+    console.log(postData);
+    this.postData.reset();
+
+    //Send Http Request
+  }
+
+  onFetchPosts() {
+    //Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
